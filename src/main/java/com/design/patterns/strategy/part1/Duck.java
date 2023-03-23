e8dd3346f645b375450b14eab4983a94aa080e16
Rachana Devi <rachanadevi18@gmail.com>
2023-03-23 20:58:15 +0530

    Add Duck with behaviours swim, display and quack

---
+++ src/main/java/com/design/patterns/strategy/part1/Duck.java
@@ -0,0 +1,16 @@
+package com.design.patterns.strategy.part1;
+
+public class Duck {
+
+    public void swim() {
+        System.out.println("SWIM");
+    }
+
+    public void display() {
+        System.out.println("ITS ME, the duck");
+    }
+
+    public void quack() {
+        System.out.println("QUACK");
+    }
+}
