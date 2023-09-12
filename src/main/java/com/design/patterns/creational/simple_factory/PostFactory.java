3a4520c23573a2b6081004847ecbea68529cf1de
Rachana Devi <rachanadevi18@gmail.com>
2023-09-12 23:18:53 +0530

    Rachana | Create PostFactory which has implement SimpleFactory design pattern

--- src/main/java/com/design/patterns/creational/simple_factory/PostFactory.java
+++ src/main/java/com/design/patterns/creational/simple_factory/PostFactory.java
@@ -1,4 +1,14 @@
 package com.design.patterns.creational.simple_factory;
 
 public class PostFactory {
+
+    // we will create simple factory
+    public static Post createPost(String type) {
+        return switch (type) {
+            case "news" -> new NewsPost();
+            case "blog" -> new BlogPost();
+            case "product" -> new ProductPost();
+            default -> throw new IllegalArgumentException("Post type is unknown");
+        };
+    }
 }
